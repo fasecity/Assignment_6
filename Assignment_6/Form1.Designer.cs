b86c6ee619b3ba667203682da843351974b9f64e
fasecity <audiotrash112@gmail.com>
2016-08-02 02:30:28 -0400

    commit

--- Assignment_6/Form1.Designer.cs
+++ Assignment_6/Form1.Designer.cs
@@ -1,6 +1,8 @@
-﻿namespace Assignment_6
+﻿using System.Windows.Forms;
+
+namespace Assignment_6
 {
-    partial class Form1
+    partial class BmiCalculator
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +30,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.MetricradioButton1 = new System.Windows.Forms.RadioButton();
+            this.SpitRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.Heightlabel = new System.Windows.Forms.Label();
+            this.WeightLabel = new System.Windows.Forms.Label();
+            this.ImperialradioButton2 = new System.Windows.Forms.RadioButton();
+            this.HeightTextBox = new System.Windows.Forms.TextBox();
+            this.WeightTextBox = new System.Windows.Forms.TextBox();
+            this.CalculateTextBox = new System.Windows.Forms.TextBox();
+            this.ClearButon = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Calculate
+            // 
+            this.Calculate.Location = new System.Drawing.Point(44, 248);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(148, 64);
+            this.Calculate.TabIndex = 1;
+            this.Calculate.Text = "Calculate";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // MetricradioButton1
+            // 
+            this.MetricradioButton1.AutoSize = true;
+            this.MetricradioButton1.Location = new System.Drawing.Point(44, 22);
+            this.MetricradioButton1.Name = "MetricradioButton1";
+            this.MetricradioButton1.Size = new System.Drawing.Size(77, 24);
+            this.MetricradioButton1.TabIndex = 3;
+            this.MetricradioButton1.TabStop = true;
+            this.MetricradioButton1.Text = "Metric";
+            this.MetricradioButton1.UseVisualStyleBackColor = true;
+            this.MetricradioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // SpitRichTextBox
+            // 
+            this.SpitRichTextBox.Location = new System.Drawing.Point(44, 337);
+            this.SpitRichTextBox.Name = "SpitRichTextBox";
+            this.SpitRichTextBox.Size = new System.Drawing.Size(175, 74);
+            this.SpitRichTextBox.TabIndex = 5;
+            this.SpitRichTextBox.Text = "";
+            this.SpitRichTextBox.TextChanged += new System.EventHandler(this.Calculate_Click);
+            // 
+            // Heightlabel
+            // 
+            this.Heightlabel.AutoSize = true;
+            this.Heightlabel.Location = new System.Drawing.Point(65, 95);
+            this.Heightlabel.Name = "Heightlabel";
+            this.Heightlabel.Size = new System.Drawing.Size(56, 20);
+            this.Heightlabel.TabIndex = 8;
+            this.Heightlabel.Text = "Height";
+            // 
+            // WeightLabel
+            // 
+            this.WeightLabel.AutoSize = true;
+            this.WeightLabel.Location = new System.Drawing.Point(70, 169);
+            this.WeightLabel.Name = "WeightLabel";
+            this.WeightLabel.Size = new System.Drawing.Size(59, 20);
+            this.WeightLabel.TabIndex = 9;
+            this.WeightLabel.Text = "Weight";
+            // 
+            // ImperialradioButton2
+            // 
+            this.ImperialradioButton2.AutoSize = true;
+            this.ImperialradioButton2.Location = new System.Drawing.Point(307, 22);
+            this.ImperialradioButton2.Name = "ImperialradioButton2";
+            this.ImperialradioButton2.Size = new System.Drawing.Size(90, 24);
+            this.ImperialradioButton2.TabIndex = 4;
+            this.ImperialradioButton2.TabStop = true;
+            this.ImperialradioButton2.Text = "Imperial";
+            this.ImperialradioButton2.UseVisualStyleBackColor = true;
+            this.ImperialradioButton2.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // HeightTextBox
+            // 
+            this.HeightTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HeightTextBox.Location = new System.Drawing.Point(212, 95);
+            this.HeightTextBox.Name = "HeightTextBox";
+            this.HeightTextBox.Size = new System.Drawing.Size(221, 53);
+            this.HeightTextBox.TabIndex = 10;
+            // 
+            // WeightTextBox
+            // 
+            this.WeightTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WeightTextBox.Location = new System.Drawing.Point(212, 169);
+            this.WeightTextBox.Name = "WeightTextBox";
+            this.WeightTextBox.Size = new System.Drawing.Size(221, 53);
+            this.WeightTextBox.TabIndex = 11;
+            // 
+            // CalculateTextBox
+            // 
+            this.CalculateTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CalculateTextBox.Location = new System.Drawing.Point(212, 248);
+            this.CalculateTextBox.Name = "CalculateTextBox";
+            this.CalculateTextBox.Size = new System.Drawing.Size(221, 53);
+            this.CalculateTextBox.TabIndex = 12;
+            // 
+            // ClearButon
+            // 
+            this.ClearButon.Location = new System.Drawing.Point(307, 337);
+            this.ClearButon.Name = "ClearButon";
+            this.ClearButon.Size = new System.Drawing.Size(139, 74);
+            this.ClearButon.TabIndex = 13;
+            this.ClearButon.Text = "Clear";
+            this.ClearButon.UseVisualStyleBackColor = true;
+            this.ClearButon.Click += new System.EventHandler(this.ClearButon_Click);
+            // 
+            // BmiCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(458, 446);
+            this.Controls.Add(this.ClearButon);
+            this.Controls.Add(this.CalculateTextBox);
+            this.Controls.Add(this.WeightTextBox);
+            this.Controls.Add(this.HeightTextBox);
+            this.Controls.Add(this.WeightLabel);
+            this.Controls.Add(this.Heightlabel);
+            this.Controls.Add(this.SpitRichTextBox);
+            this.Controls.Add(this.ImperialradioButton2);
+            this.Controls.Add(this.MetricradioButton1);
+            this.Controls.Add(this.Calculate);
+            this.Name = "BmiCalculator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BMI Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Button Calculate;
+        private RadioButton MetricradioButton1;
+        private System.Windows.Forms.RichTextBox SpitRichTextBox;
+        private System.Windows.Forms.Label Heightlabel;
+        private System.Windows.Forms.Label WeightLabel;
+        private RadioButton ImperialradioButton2;
+        private TextBox HeightTextBox;
+        private TextBox WeightTextBox;
+        private TextBox CalculateTextBox;
+        private Button ClearButon;
     }
 }
 
--- Assignment_6/Form1.cs
+++ Assignment_6/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,111 @@ using System.Windows.Forms;
 
 namespace Assignment_6
 {
-    public partial class Form1 : Form
+    public partial class BmiCalculator : Form
     {
-        public Form1()
+        private bool _isMetric;
+        public BmiCalculator()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MetricradioButton1.Checked = true;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rdo = (RadioButton)sender;
+            if (rdo == MetricradioButton1)
+            {
+                Heightlabel.Text = "Cm";
+                WeightLabel.Text = "KG";
+                _isMetric = true;
+            }
+
+            else
+            {
+                Heightlabel.Text = "In";
+                WeightLabel.Text = "Lbs";
+                _isMetric = false;
+            }
+        }
+
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double weight = Convert.ToDouble(WeightTextBox.Text);
+                double height = Convert.ToDouble(HeightTextBox.Text);
+                double bmi = weight / (height * height);
+                if (!_isMetric)
+                {
+                    bmi *= 703;
+
+                    if (bmi <= 18.5)
+                    {
+                        SpitRichTextBox.Text = "Underweight";
+
+                    }
+                    else if (bmi >= 18.5 && bmi <= 24.9)
+                    {
+                        SpitRichTextBox.Text = "Normal";
+
+                    }
+                    else if (bmi >= 25 && bmi <= 29.9)
+                    {
+                        SpitRichTextBox.Text = "Overweight";
+                    }
+                    else
+                    {
+                        SpitRichTextBox.Text = "Obese";
+                    }
+
+                }
+                CalculateTextBox.Text = bmi.ToString();
+                if (_isMetric)
+                {
+                    bmi *= 703;
+
+                    if (bmi <= 18.5)
+                    {
+                        SpitRichTextBox.Text = "Underweight";
+
+                    }
+                    else if (bmi >= 18.5 && bmi <= 24.9)
+                    {
+                        SpitRichTextBox.Text = "Normal";
+
+                    }
+                    else if (bmi >= 25 && bmi <= 29.9)
+                    {
+                        SpitRichTextBox.Text = "Overweight";
+                    }
+                    else
+                    {
+                        SpitRichTextBox.Text = "Obese";
+                    }
+                }
+                CalculateTextBox.Text = bmi.ToString();
+
+            }
+            catch (FormatException error)
+            {
+               
+                MessageBox.Show($"Please enter Nunbers! {0}", error.Message);
+            }
+            
+
+        }
+
+        private void ClearButon_Click(object sender, EventArgs e)
+        {
+            HeightTextBox.Text = "";
+            WeightTextBox.Text = "";
+            CalculateTextBox.Text = "";
+            SpitRichTextBox.Text = "";
+        }
     }
-}
+    }
+
